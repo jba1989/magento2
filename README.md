a04bff6a3f2a77513f92998e0ea86388b39ffa43
root <root@li1562-54.members.linode.com>
2021-05-30 14:12:06 +0000

    open api test - exchange rate

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # hello world module test
 http://{ domain_name }/helloworld/index/test
+
+# open api test - exchange rate from <https://tw.rter.info/howto_currencyapi.php>
+http://{ domain_name }/helloworld/index/display
---
+++ app/code/Mageplaza/HelloWorld/Block/Display.php
@@ -0,0 +1,24 @@
+<?php
+namespace Mageplaza\HelloWorld\Block;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientFactory;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ResponseFactory;
+
+class Display extends \Magento\Framework\View\Element\Template
+{
+	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	{
+		parent::__construct($context);
+	}
+
+    public function showExchangeRate()
+	{
+		$client = new Client();
+        $response = $client->get('https://tw.rter.info/capi.php');
+        
+        return $response->getBody();
+	}
+}
